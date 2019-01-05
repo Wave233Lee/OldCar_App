e36e2b103bb7b65b6ba35ec20c13d51637460e47
msl5266 <45361106+msl5266@users.noreply.github.com>
2019-01-05 23:04:12 +0800

    oldcar

--- src/main/java/com/example/group/fragment/View4Fragment.java
+++ src/main/java/com/example/group/fragment/View4Fragment.java
@@ -2,9 +2,12 @@ package com.example.group.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,16 +25,18 @@ public class View4Fragment extends Fragment implements View.OnClickListener,
         ViewPager.OnPageChangeListener {
     protected View mView;
     protected Context mContext;
-    private ViewPager vpager_two;
+    private ViewPager vpager_four;
     private ImageView img_cursor;
     private TextView tv_one;
     private TextView tv_two;
+    private TextView tv_three;
     private ArrayList<View> listViews;
     private int offset = 0;//移动条图片的偏移量
     private int currIndex = 0;//当前页面的编号
     private int bmpWidth;// 移动条图片的长度
     private int one = 0; //移动条滑动一页的距离
     private int two = 0; //滑动条移动两页的距离
+    private  int three=0;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         mContext=getActivity();
@@ -41,38 +46,40 @@ public class View4Fragment extends Fragment implements View.OnClickListener,
     }
 
     private void initViews() {
-        vpager_two = (ViewPager) mView.findViewById(R.id.vpager_two1);
+        vpager_four = (ViewPager) mView.findViewById(R.id.vpager_four);
         tv_one = (TextView) mView.findViewById(R.id.tv_one);
         tv_two = (TextView) mView.findViewById(R.id.tv_two);
+        tv_three = (TextView) mView.findViewById(R.id.tv_three);
         img_cursor = (ImageView) mView.findViewById(R.id.img_cursor);
-
-
 
 
 
         //往ViewPager填充View，同时设置点击事件与页面切换事件
         listViews = new ArrayList<View>();
         LayoutInflater mInflater = getLayoutInflater();
-        listViews.add(mInflater.inflate(R.layout.view5, null, false));
-        listViews.add(mInflater.inflate(R.layout.view6, null, false));
-        vpager_two.setAdapter(new MyPagerAdapter(listViews));
-        vpager_two.setCurrentItem(0);          //设置ViewPager当前页，从0开始算
+        listViews.add(mInflater.inflate(R.layout.view9, null, false));
+        listViews.add(mInflater.inflate(R.layout.view10, null, false));
+        //listViews.add(mInflater.inflate(R.layout.view3, null, false));
+        vpager_four.setAdapter(new MyPagerAdapter(listViews));
+        vpager_four.setCurrentItem(0);          //设置ViewPager当前页，从0开始算
 
         tv_one.setOnClickListener(this);
         tv_two.setOnClickListener(this);
+        //tv_three.setOnClickListener(this);
 
-        vpager_two.addOnPageChangeListener(this);
+        vpager_four.addOnPageChangeListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_one:
-                vpager_two.setCurrentItem(0);
+                vpager_four.setCurrentItem(0);
                 break;
             case R.id.tv_two:
-                vpager_two.setCurrentItem(1);
+                vpager_four.setCurrentItem(1);
                 break;
+
         }
     }
 
@@ -81,10 +88,31 @@ public class View4Fragment extends Fragment implements View.OnClickListener,
         Animation animation = null;
         switch (index) {
             case 0:
-                animation = new TranslateAnimation(one, 0, 0, 0);
+                if (currIndex == 1) {
+                    animation = new TranslateAnimation(one, 0, 0, 0);
+                } else if (currIndex == 2) {
+                    animation = new TranslateAnimation(two, 0, 0, 0);
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(three, 0, 0, 0);
+                }
                 break;
             case 1:
-                animation = new TranslateAnimation(offset, one, 0, 0);
+                if (currIndex == 0) {
+                    animation = new TranslateAnimation(offset, one, 0, 0);
+                } else if (currIndex == 2) {
+                    animation = new TranslateAnimation(two, one, 0, 0);
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(three, one, 0, 0);
+                }
+                break;
+            case 2:
+                if (currIndex == 0) {
+                    animation = new TranslateAnimation(offset, two, 0, 0);
+                } else if (currIndex == 1) {
+                    animation = new TranslateAnimation(one, two, 0, 0);
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(three, two, 0, 0);
+                }
                 break;
         }
         currIndex = index;
--- src/main/java/com/example/group/fragment/View9Fragment.java
+++ src/main/java/com/example/group/fragment/View9Fragment.java
@@ -1,36 +1,166 @@
 package com.example.group.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.group.R;
-import com.example.group.adapter.PlanetAdapter;
-import com.example.group.bean.Planet;
+import com.example.group.util.HttpCallbackListener;
+import com.example.group.util.HttpUtil;
+import com.example.group.widget.CarActivity;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.os.Handler;
 
-public class View9Fragment extends Fragment {
+
+public class View9Fragment extends Fragment implements AdapterView.OnItemClickListener {
     protected View mView;
     protected Context mContext;
-    private ListView lv_planet;
-    private ArrayList<Planet> planetList;
+    private GridView gridView;
+    private ImageView image1;
+    private ImageView image2;
+    private SimpleAdapter adapter;
+    private List<Map<String, Object>> dataList;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         mContext=getActivity();
-        mView=inflater.inflate(R.layout.view5_fragment,container,false);
-        planetList = Planet.getDefaultList();
-        PlanetAdapter adapter = new PlanetAdapter(this.mContext, R.layout.item_list, planetList, Color.WHITE);
-        lv_planet = (ListView) mView.findViewById(R.id.lv_planet);
-        lv_planet.setAdapter(adapter);
-        lv_planet.setOnItemClickListener(adapter);
-        lv_planet.setOnItemLongClickListener(adapter);
+        mView=inflater.inflate(R.layout.view9fragment,container,false);
+        gridView=(GridView) mView.findViewById(R.id.gridView);
+        image1=mView.findViewById(R.id.image1);
+        image2=mView.findViewById(R.id.image2);
+        getMessage(mView);
+        dataList=new ArrayList<Map<String,Object>>();
+        adapter=new SimpleAdapter(this.mContext, getData(), R.layout.item_view2, new String[]{"pic","name"}, new int[]{R.id.pic,R.id.name});
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(this);
         return mView;
     }
 
+    private List<Map<String, Object>> getData() {
+
+        int[] drawable = { R.drawable.trip_01, R.drawable.trip_02,
+                R.drawable.trip_03, R.drawable.trip_04,R.drawable.trip_01, R.drawable.trip_02,};
+        String[] iconName = { "灯光", "轮子", "内饰", "刹车系统", "动力系统", "悬挂系统" };
+        for (int i = 0; i < drawable.length; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("pic", drawable[i]);
+            map.put("name", iconName[i]);
+            dataList.add(map);
+        }
+        return dataList;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent=new Intent(this.mContext,CarActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String result;
+            try {
+                JSONObject jsonObject = new JSONObject(msg.obj.toString());
+                int code = jsonObject.getInt("code");
+                Log.d("a_log","code:  "+code);
+                if (code == 0){
+                    result = "登陆成功";
+                    Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
+                    JSONObject data = jsonObject.getJSONObject("data");
+
+                    String avatar_url = data.getJSONObject("avatar").getString("path");
+                    Log.d("a_log","传过来的"+avatar_url);
+                    Bitmap pic = getImage(avatar_url);
+                    if (pic != null) {
+                        Log.d("a_log","获取的图片"+pic.toString());
+                        pic = Bitmap.createScaledBitmap(pic,100,100,true);
+                    }else
+                        Log.d("a_log","none图片");
+                    image1.setImageBitmap(pic);
+
+                }else {
+                    result = "获取失败";
+                    Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    public static Bitmap getImage(String path){
+
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(path).openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            Log.d("a_log","链接的URL"+conn.getURL().toString());
+            if(conn.getResponseCode() == 200){
+                InputStream inputStream = conn.getInputStream();
+                Log.d("a_log","实际链接的"+inputStream.toString());
+                return BitmapFactory.decodeStream(inputStream);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void getMessage(View v) {
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("id","1");
+        try {
+            //构造完整URL
+            String originAddress = "http://111.230.34.50:8080/oldcar/user/getById";
+            String completedURL = HttpUtil.makeURL(originAddress, params);
+            Log.d("a_log","that's completedUrl:"+completedURL);
+            //发送请求
+            HttpUtil.sendGETRequest(completedURL, new HttpCallbackListener() {
+                @Override
+                public void OnFinish(String response) {
+                    Message message = new Message();
+                    message.obj = response;
+                    handler.sendMessage(message);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.d("a_log","error:"+e.toString());
+                    Message message = new Message();
+                    message.obj = e.toString();
+                    handler.sendMessage(message);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
